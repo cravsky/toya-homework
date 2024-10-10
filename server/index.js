f2b90717e18e135e536bae8877b3bcdc3cecfa43
cravsky <cravsky@gmail.com>
2024-10-10 11:38:35 +0200

    Implemented proxy server

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const request = require("request");
+
+const app = express();
+
+app.use(cors());
+
+// Endpoint proxy for OData
+app.get("/odata/*", (req, res) => {
+  const url = `https://services.odata.org/V2/Northwind/Northwind.svc${req.url.replace('/odata', '')}`;
+  req.pipe(request(url)).pipe(res);
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Proxy server running on PORT: ${PORT}`);
+});
